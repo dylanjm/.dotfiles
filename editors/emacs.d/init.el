c7d55d887abb931556d9b4aa455c078866778e57
Dylan McDowell <dylanjm@users.noreply.github.com>
2019-08-21 11:21:49 -0600

    Emacs Config

--- editors/emacs.d/init.el
+++ editors/emacs.d/init.el
@@ -1,5 +1,9 @@
 (let ((gc-cons-threshold most-positive-fixnum))
 
+  (defconst user-emacs-modules-directory
+  (expand-file-name (concat user-emacs-directory "lisp/"))
+  "Directory for storing modules.")
+
   ;; Set repositories
   (require 'package)
   (setq-default
@@ -7,6 +11,7 @@
    package-enable-at-startup nil)
   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/") t )
   (package-initialize)
 
   ;; Install dependencies
@@ -15,30 +20,18 @@
     (package-install 'use-package t))
   (setq-default
    use-package-always-defer t
-   use-package-always-ensure t)
+   use-package-always-ensure t
+   use-package-vervose t)
 
   ;; Use latest Org
   (use-package org :ensure org-plus-contrib)
+  (require 'org-tempo)
+
+  ;; Useful Modules
+  (add-to-list 'load-path user-emacs-modules-directory)
+  (require 'init-modeline)
 
   ;; Tangle configuration
   (org-babel-load-file (expand-file-name "dotemacs.org" user-emacs-directory))
   (garbage-collect))
 
-;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-archives
-   '(("org" . "https://orgmode.org/elpa/")
-     ("melpa" . "https://melpa.org/packages/")
-     ("gnu" . "https://elpa.gnu.org/packages/")))
- '(package-selected-packages
-   '(smartparens rainbow-delimiters company-auctex all-the-icons leaf-keywords use-package org-plus-contrib leaf gruvbox-theme doom-themes)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
