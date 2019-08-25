5c6536e927e35a94bfa0d486c413f38c1f43aece
Dylan McDowell <dylanjm@users.noreply.github.com>
2019-08-25 16:50:52 -0600

    Had to add back in Zprofile

--- shells/zsh/zshenv.base.zsh
+++ shells/zsh/zshenv.base.zsh
@@ -10,7 +10,7 @@ export EDITOR='emacsclient -c'
 export ALTERNATE_EDITOR=""
 export LANG=en_US.UTF-8
 
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$HOME/.pyenv/bin:$DOTFILES/bin:/Library/TeX/texbin"
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$HOME/.pyenv/bin:$DOTFILES/bin:/opt/tinytex/bin"
 
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
